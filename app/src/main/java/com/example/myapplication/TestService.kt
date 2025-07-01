7563c5b5247c07a98bec7aac547aad50e81753bb
Tiến Dzũng Nguyễn <>
2025-07-01 21:45:49 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/TestService.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+/**
+ * Created by FPL on 09/05/2025.
+ */
+ 
+ class TestService {
+}
---
+++ app/src/main/java/com/example/myapplication/language/StringApp.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication.language
+
+data class StringApp()
